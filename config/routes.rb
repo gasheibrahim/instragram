78fa7c9e3c9eb2567b20bd59d893bbcfc64a5439
gashema ibrahim <ibrahimgashema154@gmail.com>
2019-09-04 17:20:16 +0000

    sixth commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :sessions
   resources :posts
   resources :users
-
   resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
